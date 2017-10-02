f5a6872a2a8239701842bba37fe91de44a11d107
Ritesh Kumar Reddy Kuchukulla <rithu.ritesh@gmail.com>
2017-10-02 17:55:49 -0400

    Flask app.

---
+++ Webapplication/webapp.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+@app.route('/error')
+def error():
+    return render_template('404.html')
+
+if __name__=='__main__':
+   app.run(debug=True)
+   #print (app.url_map)
