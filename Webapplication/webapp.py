05e37b74aa4f76fd3de61f9b99ec7cd4d4cf6930
ritesh007 <rithu.ritesh@gmail.com>
2018-03-17 23:41:47 -0400

    web application content and modifications

--- Webapplication/webapp.py
+++ Webapplication/webapp.py
@@ -11,6 +11,10 @@ def index():
 def error():
     return render_template('404.html')
 
+@app.route('/test')
+def test():
+    return render_template('test.html')
+
 if __name__=='__main__':
    app.run(debug=True)
    #print (app.url_map)
